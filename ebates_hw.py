9b500c73a0405a23b10c146627d1fe68ff8bec07
bdc11 <31024885+bdc11@users.noreply.github.com>
2017-08-18 12:20:35 -0700

    Update ebates_hw.py

--- ebates_hw.py
+++ ebates_hw.py
@@ -7,11 +7,11 @@ hd_false = 0
 
 while more == True:
 	#print i 
-	new_url = 'http://api.viki.io/v4/videos.json?app=100250a&per_page=500&page=' + str(i) #define url and iterate it using type casting
+	new_url = 'http://api.viki.io/v4/videos.json?app=100250a&per_page=500&page=' + str(i) #define url and increment page number
 	ebates = requests.get(new_url, headers={'User-Agent': 'Mozilla/5.0'}) #connect to ebates server
-	result = json.loads(ebates.content) #decode json object into a readable python object
-	more = result['more'] #first check to see if more = true then continue
-	for keys in result['response']: #loop to iterate keys present in response dictionary
+	result = json.loads(ebates.content) #parse JSON object into an python object with attributes corresponding to dict keys
+	more = result['more'] #check if more = true then continue
+	for keys in result['response']: #loop iterates keys in response dictionary
 		flags = keys['flags'] #finds flag key values where hd is nested
 		hd = flags['hd'] #checks hd key value 
 		if hd == True: #loop checks if hd value is true 
